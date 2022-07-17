9997060a82dedcba14af8947cfaa7aa96ed46d08
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-07-17 19:00:44 -0300

    using mongodb

--- restapi/server.js
+++ restapi/server.js
@@ -2,4 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+mongoose.connect("mongodb://localhost:37017/subscribers")
+const db = mongoose.connection
+db.on('error', (err) => console.log(err))
+db.once('open',()=> console.log('Database Connected'))
+
+
 app.listen(3000, () => console.log('Server express running: http://localhost:3000'))
